c679dfa617bee5a8f1e39e9e03ce1608cca6a595
Bertherat Guillaume <bertherat.guillaume@gmail.com>
2021-09-27 14:00:56 +0200

    Remade the page style

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,28 @@
   <v-app>
     <v-main>
       <div class="main">
-        <Header />
         <div class="container">
           <Presentation />
+        </div>
+        <div class="container">
+
         </div>
         <Footer />
       </div>
     </v-main>
+
   </v-app>
 </template>
 
 <script>
 import '../src/assets/styles/master.css';
 import '../src/assets/styles/animations.css';
-import Header from "./components/Header.vue";
 import Presentation from "./components/Presentation";
 import Footer from "./components/Footer";
 
 export default {
   name: 'App',
   components:{
-    Header,
     Presentation,
     Footer
   }
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -2,7 +2,7 @@
 <div class="presentation">
   <img class="picture" alt="Photo de moi" :src="require('@/assets/images/me.jpg')">
   <div class="texts">
-    <p class="text1">Salut! Je m'appelle Guillaume Bertherat, étudiant en Master Informatique.</p>
+    <p class="text1"><b>Salut!</b> Je m'appelle Guillaume Bertherat, étudiant en Master Informatique.</p>
     <p class="text2">Passionné par l'informatique depuis petit, j'aimerais développer les outils informatiques de demain.</p>
   </div>
 </div>
